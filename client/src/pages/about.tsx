b435c5315533fada6bf640517270496273bebd94
franklinnot <franklinnot@outlook.com>
2025-04-28 19:20:05 -0500

    feat: add initial components and pages for client app
    Add basic components and pages including button, home, and about pages. Include a new logo asset and remove the old react.svg file to set up the initial structure of the client application.

---
+++ client/src/pages/about.tsx
@@ -0,0 +1,11 @@
+
+/**
+ * Represents the about page component.
+ */
+export default function Home() {
+  return (
+    <div>
+      <h1>Soy el about</h1>
+    </div>
+  );
+}
---
+++ client/src/pages/home.tsx
@@ -0,0 +1,11 @@
+
+/**
+ * Represents the home page component.
+ */
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+    </div>
+  );
+}
